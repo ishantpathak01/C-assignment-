f5aad37060ac0643f10062188d4b9b0bc53d09c9
Ishant Pathak <ishantpathak25@gmail.com>
2025-10-19 09:49:51 +0530

    Add header file for number functions

---
+++ ishantlibrary.h
@@ -0,0 +1,8 @@
+#ifndef ISHANT_H
+#define ISHANT_H
+// Number functions
+int reverseDigits(int n);
+int isArmstrong(int num);
+int isAdams(int num);
+int isPrime(int num);
+int isPrimePalindrome(int num);
